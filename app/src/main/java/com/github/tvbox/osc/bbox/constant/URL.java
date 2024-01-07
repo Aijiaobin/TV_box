6d3531858b13f7e3920db3b36b58c007284a3faa
Bing'home <wt08151217@gmail.com>
2024-01-07 21:11:23 +0800

    修改接口地址

--- app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
+++ app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
@@ -1,7 +1,7 @@
 package com.github.tvbox.osc.bbox.constant;
 
 public class URL {
-    public static String DOMAIN_NAME_PROXY = "";
+    public static String DOMAIN_NAME_PROXY = "https://mirror.ghproxy.com/";
     public static final String GITHUB_VERSION_PATH = "https://raw.githubusercontent.com/mlabalabala/box/main/version.json";
     public static final String APK_PATH = "https://raw.githubusercontent.com/mlabalabala/box/main/Release/app-release.apk";
     // public static final String DOMAIN_NAME_PROXY = "http://192.168.3.159:81";
@@ -12,10 +12,10 @@ public class URL {
     // https://ghproxy.com/https://raw.githubusercontent.com//mlabalabala/TVResource/main/boxCfg/ori_source.json
     // https://raw.bunnylblbblbl.eu.org/https://raw.githubusercontent.com/mlabalabala/TVResource/main/boxCfg/ori_source.json
     // 默认线路
-    public static final String DEFAULT_API_URL = "http://bing.x3322.net:5678/tvbox/my_ext.json";
+    public static final String DEFAULT_API_URL = "https://raw.githubusercontent.com/mlabalabala/TVResource/main/boxCfg/default";
     // public static final String DEFAULT_API_URL = "https://raw.githubusercontent.com/gaotianliuyun/gao/master/0821.json";
     // 默认仓库线路
-    public static final String DEFAULT_STORE_API_URL = "http://bing.x3322.net:5678/tvbox/my_ext.json";
+    public static final String DEFAULT_STORE_API_URL = "https://raw.githubusercontent.com/mlabalabala/TVResource/main/boxCfg/ori_source.json";
     // 默认直播线路
     public static final String DEFAULT_LIVE_URL = "https://raw.githubusercontent.com/mlabalabala/TVResource/main/boxCfg/live.txt";
 
